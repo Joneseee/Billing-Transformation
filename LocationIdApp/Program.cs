57286f3ec8a08e8de10c3c9f8d628dc4e9f01c51
Joneseee <sjjonesjr@gmail.com>
2024-09-27 10:25:04 -0500

    Add files via upload

---
+++ LocationIdApp/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Windows;
+using IntegrationLogic;
+using IntegrationLogic.Helpers;
+using IntegrationLogic.Models;
+using schemas.appliedsystems.com.epic.sdk._2017._02._account;
+
+namespace LocationIdApp;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        CommandLineArgs arguments = ArgumentsHelper.GetCommandLineArgs();
+
+        if (arguments.ContactId == null)
+        {
+            MessageBox.Show(
+                $"Location ID Generator can only be launched from the Contact View.",
+                "Error", MessageBoxButton.OK,
+                MessageBoxImage.Error, MessageBoxResult.None, MessageBoxOptions.DefaultDesktopOnly);
+        }
+        else
+        {
+            try
+            {
+                string user = string.Empty;
+
+                if (arguments.User != null)
+                {
+                    user = arguments.User;
+                }
+
+                Config config = ConfigFileHelper.ReadFile();
+
+                if (config is { DatabaseName: not null, AuthenticationKey: not null, Url: not null })
+                {
+                    SdkHelper sdkHelper = new(config.Url, config.DatabaseName, config.AuthenticationKey, user);
+
+                    Contact contact = sdkHelper.GetContact(Convert.ToInt32(arguments.ContactId)).GetAwaiter().GetResult();
+                    string siteId = $"{SiteIdGenerator.Generate10DigitNumber(contact.AccountID, contact.ContactID)}";
+
+
+                    bool updated = false;
+                    bool notNull = false;
+                    string sdkException = string.Empty;
+
+                    try
+                    {
+                        if (string.IsNullOrEmpty(contact.SiteID))
+                        {
+                            contact.SiteID = siteId;
+                            sdkHelper.UpdateContact(contact).GetAwaiter().GetResult();
+                            updated = true;
+                            Clipboard.SetText(siteId);
+                        }
+                        else
+                        {
+                            notNull = true;
+                        }
+                        
+                    }
+                    catch(Exception ex)
+                    {
+                        updated = false;
+                        sdkException = ex.Message;
+                    }
+
+                    if (updated)
+                    {
+                        MessageBox.Show(
+                            $"Site ID has been generated for the selected contact and copied to clipboard.{Environment.NewLine}{Environment.NewLine}Site ID = {siteId}",
+                            "Success", MessageBoxButton.OK,
+                            MessageBoxImage.Information, MessageBoxResult.None, MessageBoxOptions.DefaultDesktopOnly);
+                    }
+                    else
+                    {
+                        MessageBox.Show(
+                            notNull
+                                ? $"Site ID is not blank."
+                                : $"An error occurred while updating Site ID:{Environment.NewLine}{sdkException}",
+                            "Error", MessageBoxButton.OK,
+                            MessageBoxImage.Error, MessageBoxResult.None, MessageBoxOptions.DefaultDesktopOnly);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"An error occurred while updating Site ID for the selected Contact:{Environment.NewLine}{ex.Message}",
+                    "Error", MessageBoxButton.OK,
+                    MessageBoxImage.Error, MessageBoxResult.None, MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
+        
+    }
+}
---
+++ MajescoReIndexApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.SqlClient;
+using System.Diagnostics;
+using IntegrationLogic;
+using IntegrationLogic.Helpers;
+using IntegrationLogic.Models;
+
+namespace MajescoReIndexApp;
+
+class Program
+{
+    private static Config? _config; 
+    private static BdeHelper? _bdeHelper;
+    private static readonly Mutex Mutex = new(true, "F6133E37-F0FE-4C28-BBE9-77BF6C002C9B");
+
+    [STAThread]
+    private static void Main()
+    {
+        if (Mutex.WaitOne(TimeSpan.FromSeconds(30), true))
+        {
+            // Elapsed process time
+            Stopwatch timer = new();
+            timer.Start();
+
+            // Added due to issues with application freezing during processing for Roanoke. With Quick Edit enabled, clicking into the console application could terminate the process.
+            ConsoleWindow.QuickEditMode(false);
+
+            // Read config file
+            Console.WriteLine("Reading configuration file.");
+
+            _config = ConfigFileHelper.ReadFile();
+            if (string.IsNullOrEmpty(_config.AuthenticationKey) || string.IsNullOrEmpty(_config.FilePath) || string.IsNullOrEmpty(_config.Url) || string.IsNullOrEmpty(_config.DatabaseName))
+            {
+                EventLog.WriteEntry(".NET Runtime", "Process cannot be started. Config file is blank or incomplete.", EventLogEntryType.Error, 1000);
+                return;
+            }
+
+            // Reindex Database
+            Console.WriteLine("Reindexing database.");
+            _bdeHelper = new(_config.SqlServer!, _config.SqlDatabase!, _config.SqlUsername!, _config.SqlPassword!);
+
+            // Open SQL Connection
+            using SqlConnection sqlConnection = new(_bdeHelper.GetConnectionString());
+            sqlConnection.Open();
+            _bdeHelper.ExecuteQuery("DatabaseReindex.sql", sqlConnection);
+            sqlConnection.Close();
+
+            // Total elapsed time
+            TimeSpan timeSpan = timer.Elapsed;
+            Console.WriteLine($@"Process completed in {timeSpan:hh\:mm\:ss\:fff}");
+        }
+    }
+
+};
